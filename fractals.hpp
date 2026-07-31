f5080bad066def32d411180a5f710696abd94d69
StefanoSpadano <stefano.spadano426@gmail.com>
2026-07-31 11:41:05 +0200

    Modify cubic function name to match its name in graphics.cpp

--- fractals.hpp
+++ fractals.hpp
@@ -3,8 +3,8 @@
 
 #include <SFML/Graphics.hpp>
 
-void compute_julia(sf::Image& image, int width, int height, double center_y, double center_x, double zoom)
-void compute_mandelbrot(sf::Image& image, int width, int height, double center_y, double center_x, double zoom)
-void compute_cubic(sf::Image& image, int width, int height, double center_y, double center_x, double zoom) 
+void compute_julia(sf::Image& image, int width, int height, double center_x, double center_y, double zoom)
+void compute_mandelbrot(sf::Image& image, int width, int height, double center_x, double center_y, double zoom)
+void compute_cubic_fractal(sf::Image& image, int width, int height, double center_x, double center_y, double zoom) 
 
 #endif
